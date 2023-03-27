10a61c36a02d5c2cfea0ab01ebf753ea2a9ab88b
biplab <biplab.chandradey@gmail.com>
2023-03-27 09:03:12 +0530

    This is my first commit

---
+++ mathfunctionlibrary/src/main/java/com/example/mathfunctionlibrary/CommonMathFunction.kt
@@ -0,0 +1,19 @@
+package com.example.mathfunctionlibrary
+
+public class CommonMathFunction {
+    public fun addition(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    public fun subtraction(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    public fun multiplication(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    public fun division(a: Int, b: Int): Int {
+        return a / b
+    }
+}
